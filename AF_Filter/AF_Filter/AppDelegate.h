855887c0501e976fefdebe03bb9e3fc908ecef12
lostjared <jaredbruni@gmail.com>
2016-04-11 04:14:52 -0700

    Added files via upload
    current version

---
+++ AF_Filter/AF_Filter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AF_Filter
+//
+//  Created by Jared Bruni on 10/20/13.
+//  Copyright (c) 2013 Jared Bruni. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ AF_Filter/AF_Filter/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  AF_Filter
+//
+//  Created by Jared Bruni on 10/20/13.
+//  Copyright (c) 2013 Jared Bruni. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
